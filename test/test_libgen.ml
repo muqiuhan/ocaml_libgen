93f7bda6e240bbf82df76d4d17aa85affcb4665b
Muqiu Han <muqiu-han@outlook.com>
2023-06-03 23:46:26 +0800

    Improve test message

--- test/test_libgen.ml
+++ test/test_libgen.ml
@@ -2,19 +2,19 @@ let _test_dirname =
   let _success =
     let dirname_ret = Libgen.dirname "/home/muqiu-han/xxx.ml" in
     if String.equal dirname_ret "/home/muqiu"
-    then print_endline {| test (Libgen.dirname "/home/muqiu-han/xxx.ml")...ok!!! |}
+    then print_endline {|o- test (Libgen "/home/muqiu-han/xxx.ml")...ok |}
     else
       print_endline
-        ({| test dirname...failure!!!\n -> Libgen.dirname "/home/muqiu-han/xxx.ml" = |}
+        ({|o- test (Libgen "/home/muqiu-han/xxx.ml")...failure | Libgen.dirname "/home/muqiu-han/xxx.ml" = |}
          ^ dirname_ret)
   in
   let _failure =
     let dirname_ret = Libgen.dirname "xxx.ml" in
     if String.equal dirname_ret "."
-    then print_endline {|test (Libgen.dirname "xxx.ml")...ok!!!|}
+    then print_endline {|o- test (Libgen.dirname "xxx.ml")...ok!!!|}
     else
       print_endline
-        ({| test dirname...failure!!!\n -> Libgen.dirname "xxx.ml" = " |} ^ dirname_ret)
+        ({|o- test (Libgen.dirname "xxx.ml")...failure | Libgen.dirname "xxx.ml" = " |} ^ dirname_ret)
   in
   ()
 ;;
@@ -23,19 +23,19 @@ let _test_xpg_basename =
   let _success =
     let xpg_basename_ret = Libgen.xpg_basename "/home/muqiu-han/xxx.ml" in
     if String.equal xpg_basename_ret "/home/muqiu"
-    then print_endline {| test (Libgen.xpg_basename "/home/muqiu-han/xxx.ml")...ok!!! |}
+    then print_endline {|o- test (Libgen.xpg_basename "/home/muqiu-han/xxx.ml")...ok |}
     else
       print_endline
-        ({| test xpg_basename...failure!!!\n -> Libgen.xpg_basename "/home/muqiu-han/xxx.ml" = |}
+        ({|o- test (Libgen.xpg_basename "/home/muqiu-han/xxx.ml")...failure | => |}
          ^ xpg_basename_ret)
   in
   let _failure =
     let xpg_basename_ret = Libgen.xpg_basename "xxx.ml" in
     if String.equal xpg_basename_ret "."
-    then print_endline {|test (Libgen.xpg_basename "xxx.ml")...ok!!!|}
+    then print_endline {|o- test (Libgen.xpg_basename "xxx.ml")...ok|}
     else
       print_endline
-        ({| test basename...failure!!!\n -> Libgen.xpg_basename "xxx.ml" = " |}
+        ({|o- test (Libgen.xpg_basename "xxx.ml")...failure | => |}
          ^ xpg_basename_ret)
   in
   ()
