2033e7f96a88709cd9a0e162f127f2c423464169
Muqiu Han <muqiu-han@outlook.com>
2023-06-03 23:29:04 +0800

    Add c binding code

--- lib/libgen_stubs.c
+++ lib/libgen_stubs.c
@@ -1,5 +1,31 @@
 #include <libgen.h>
+
+#include <caml/alloc.h>
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/memory.h>
 #include <caml/mlvalues.h>
 
 /* Return directory part of PATH or "." if none is available.  */
-extern char *ocaml_dirname (char *__path) __THROW;
+value
+ocaml_dirname (value ocaml_path_value)
+{
+  CAMLparam1 (ocaml_path_value);
+
+  CAMLreturn (
+      caml_copy_string (dirname ((char *)(String_val (ocaml_path_value)))));
+}
+
+/* Return final component of PATH.
+
+   This is the weird XPG version of this function.  It sometimes will
+   modify its argument.  Therefore we normally use the GNU version (in
+   <string.h>) and only if this header is included make the XPG
+   version available under the real name.  */
+value
+ocaml__xpg_basename (value ocaml_path)
+{
+  CAMLparam1 (ocaml_path);
+
+  CAMLreturn (caml_copy_string (dirname ((char *)(String_val (ocaml_path)))));
+}
